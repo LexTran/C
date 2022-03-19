816f061624854b1bc2c1906d8184f451088a6500
lexonTran <525048951@qq.com>
2022-03-19 12:35:51 +0800

    modify compile

--- C/compile.sh
+++ C/compile.sh
@@ -1,16 +1,16 @@
-a=`uname -a`
+a=`uname`
 b="MINGW64_NT-10.0-19042"
 c="Darwin"
 d="ubuntu"
-if [[ $a =~ $b ]]; then
-    gcc -g ./src/$1.c -o ./run./$1.exe
+if [[ $a == $b ]]; then
+    gcc -g ./src/$1.c -o ./run/$1.exe
     cd ./run
     $1.exe
     cd ..
-elif [[ $a =~ $c ]]; then
-    gcc -g ./src/$1.c -o ./run./$1
+elif [[ $a == $c ]]; then
+    gcc -g ./src/$1.c -o ./run/$1
     ./run/$1
-elif [[ $a =~ $d ]]; then
-    gcc -g ./src/$1.c -o ./run./$1
+elif [[ $a == $d ]]; then
+    gcc -g ./src/$1.c -o ./run/$1
     ./run/$1
 fi
