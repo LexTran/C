e2887fc4c15fe7a91fcb883f41344cb7f3ba94cb
lexonTran <525048951@qq.com>
2022-03-22 21:46:57 +0800

    C

--- C/src/cp_24.c
+++ C/src/cp_24.c
@@ -1,8 +1,17 @@
 /*判断质数*/
 #include <stdio.h>
+#include <math.h>
 
 int isPrime(int n){
-    for(int i=2;i<n;i++){
+    for(int i=2;i<n/2;i++){
+        if(n%i==0)
+            return 0;
+    }
+    return 1;
+}
+int isPrime_sq(int n){
+    int j=sqrt(n+1);
+    for(int i=2;i<j;i++){
         if(n%i==0)
             return 0;
     }
@@ -18,6 +27,7 @@ int main(){
     }else{
         printf("%d is not 质数", n);
     }
-    
+    int m=isPrime_sq(n);
+    printf("\n%d", m);
     return 0;
 }
